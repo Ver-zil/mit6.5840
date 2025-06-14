0ba2bb22db337792e8b382f2f20aef4db9404f02
Verzil <1143538548@qq.com>
2025-06-14 17:41:06 +0800

    feat:finish lab2-3 "Key/value server with dropped messages" and lab2-4 "Implementing a lock using key/value clerk and unreliable network"

--- src/kvsrv1/client.go
+++ src/kvsrv1/client.go
@@ -2,12 +2,14 @@ package kvsrv
 
 import (
 	"log"
+	"time"
 
 	"6.5840/kvsrv1/rpc"
 	"6.5840/kvtest1"
 	"6.5840/tester1"
 )
 
+// 对call之类的方法进行封装，有一部分原因是为了方便在测试的时候，模拟随机丢包的问题
 type Clerk struct {
 	clnt   *tester.Clnt // 其中定义好了rpc通讯的方法call
 	server string
@@ -32,6 +34,7 @@ func MakeClerk(clnt *tester.Clnt, server string) kvtest.IKVClerk {
 // arguments. Additionally, reply must be passed as a pointer.
 func (ck *Clerk) Get(key string) (string, rpc.Tversion, rpc.Err) {
 	// You will have to modify this function.
+	// 当前存在丢包问题的时候，get操作只需要重复循环就行了，不会有什么问题
 	args := rpc.GetArgs{Key: key}
 	reply := rpc.GetReply{}
 	for !ck.clnt.Call(ck.server, "KVServer.Get", &args, &reply) {
@@ -66,11 +69,25 @@ func (ck *Clerk) Get(key string) (string, rpc.Tversion, rpc.Err) {
 // arguments. Additionally, reply must be passed as a pointer.
 func (ck *Clerk) Put(key, value string, version rpc.Tversion) rpc.Err {
 	// You will have to modify this function.
+	// 开始进行lab2-3的修改：rpc.ErrMaybe
+	// 1. client进行put的时候没有收到reply对应两种情况
+	//	第一种是request丢包了，这种就意味着没有执行
+	//	第二种是reply丢包了，这种意味着执行了
+	//	对于应用来说，并不直到到底执行还是没有执行，所以需要不停重复直到收到回复为止
+	//	第一次收到err直接将err返回，第二次还是err的话，直接返回rpc.ErrMaybe
 	args := rpc.PutArgs{Key: key, Value: value, Version: version}
 	reply := rpc.PutReply{}
 
+	isRirst := true
 	for !ck.clnt.Call(ck.server, "KVServer.Put", &args, &reply) {
-		log.Printf("client put connect err\n")
+		log.Printf("client put connect err \n")
+		isRirst = false
+		time.Sleep(100 * time.Millisecond)
+	}
+
+	// 只有多次rpc并且操作失败了，版本号不匹配，才返回rpc.ErrMaybe
+	if !isRirst && reply.Err == rpc.ErrVersion {
+		return rpc.ErrMaybe
 	}
 
 	return reply.Err
